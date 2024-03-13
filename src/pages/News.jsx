1c219b22cbf3686e8e92e62172ca7bc32a6041e6
Selani00 <pkselani00@gmail.com>
2024-03-14 00:00:04 +0530

    Add searchbar.jsx

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -4,21 +4,22 @@ import Navbar from "../components/Common/Navbar";
 import Footer from "../components/Common/Footer";
 import Emergancy from "../components/Common/Emergancy_icon";
 import image from "../assets/Footer.png";
+import SearchBar from "../components/Common/SearchBar";
 
 import News_Items from "../components/News/News_Items";
 
 const News = () => {
-  const [selectedNews,setNewsSelected]=useState(null);
+  const [selectedNews, setNewsSelected] = useState(null);
 
-  const handleSelectNews=(news)=>{
+  const handleSelectNews = (news) => {
     setNewsSelected(news);
   };
-  
+
   return (
     <>
       <LanguageBar />
       <Navbar />
-      <Emergancy/>
+      <Emergancy />
       {/* Breaking News */}
       <div className="relative w-full h-8  top-5 px-5">
         <div className="flex bg-red-600 items-center">
@@ -38,34 +39,7 @@ const News = () => {
 
       {/* Search */}
 
-      <form className="relative md:mt-12 mt-20 px-20">
-        <div class="relative">
-          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg
-              class="w-4 h-4 text-gray-500 "
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </div>
-          <input
-            type="search"
-            id="search"
-            class="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500   "
-            placeholder="Search"
-            required
-          />
-        </div>
-      </form>
+      <SearchBar />
 
       {/* News Section */}
       <div className="flex justify-between py-5 px-5 gap-5">
@@ -78,79 +52,87 @@ const News = () => {
                   {selectedNews.heading}
                 </h1>
                 <p className="my-3 text-base">{selectedNews.Auther}</p>
-                <div className="w-1/4 flex justify-between text-xs text-gray-500">
+                <div className="w-1/4 flex justify-between text-xs text-gray-500 py-4">
                   <p>{selectedNews.date}</p>
                   <p>{selectedNews.time}</p>
                 </div>
 
-                <div className="mt-5">
-                  <img src={selectedNews.image? selectedNews.image:image} alt="" className="object-cover w-full h-full" />
+                <div
+                  className="relative"
+                  style={{ width: "100%", paddingBottom: "56.25%" }}
+                >
+                  <img
+                    src={selectedNews.image ? selectedNews.image : image}
+                    alt=""
+                    className="absolute inset-0 w-full h-full object-cover"
+                    style={{ width: "100%", height: "100%" }}
+                  />
                 </div>
 
                 <div className="mt-3">
                   <p className="text-black text-sm leading-7">
                     {selectedNews.news_body}
                   </p>
-                </div>         
+                </div>
               </div>
-
             )}
           </div>
 
-            {/* Prev and Next Button */}
+          {/* Prev and Next Button */}
           <div className="flex justify-between my-10 mx-5">
-              <button
-                type="button"
-                className="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm md:px-5 px-2.5 py-2.5 text-center inline-flex items-center"
+            <button
+              type="button"
+              className="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm md:px-5 px-2.5 py-2.5 text-center inline-flex items-center"
+            >
+              <svg
+                className="w-3.5 h-3.5 me-2 transform rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
               >
-                <svg
-                  className="w-3.5 h-3.5 me-2 transform rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-                <span className="hidden sm:inline ">Previous</span>
-              </button>
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+              <span className="hidden sm:inline ">Previous</span>
+            </button>
 
-              <button
-                type="button"
-                className="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm md:px-5 px-2.5 py-2.5 text-center inline-flex items-center "
+            <button
+              type="button"
+              className="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm md:px-5 px-2.5 py-2.5 text-center inline-flex items-center "
+            >
+              <span className="hidden sm:inline">Next</span>
+              <svg
+                className="w-3.5 h-3.5 ms-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
               >
-                <span className="hidden sm:inline">Next</span>
-                <svg
-                  className="w-3.5 h-3.5 ms-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </button>
-            </div>
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-        
 
         {/* Right Section */}
         <div className="w-1/3 border border-black">
-          <News_Items onSelectedNews={(value) => {
-            handleSelectNews(value);
-          }}/>
+          <News_Items
+            onSelectedNews={(value) => {
+              handleSelectNews(value);
+            }}
+          />
         </div>
       </div>
 
